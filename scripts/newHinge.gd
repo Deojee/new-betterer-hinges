35130ed2a349bf5d7d8093dc8d609a34305c3ae7
Deojee <72935900+Deojee@users.noreply.github.com>
2026-06-20 19:16:58 -0400

    tried some stuff that didn't work

--- scripts/newHinge.gd
+++ scripts/newHinge.gd
@@ -3,6 +3,7 @@ extends Node3D
 @export var nodeA : RigidBody3D
 @export var nodeB : RigidBody3D
 
+var lastFrameTransform : Transform3D
 
 var offsetA : Transform3D
 var offsetB : Transform3D
@@ -22,6 +23,8 @@ var rotAxis : Vector3:
 
 func _ready() -> void:
 	
+	lastFrameTransform = global_transform
+	
 	#Node A in the hinge's local space
 	offsetA = global_transform.affine_inverse() * nodeA.global_transform
 	offsetB = global_transform.affine_inverse() * nodeB.global_transform
@@ -38,7 +41,7 @@ func _ready() -> void:
 
 var TPS:
 	get:
-		return Engine.physics_ticks_per_second * 0.5
+		return Engine.physics_ticks_per_second * 1.0
 	set(value):
 		print("Please set TPS in physics/common")
 
@@ -49,7 +52,7 @@ func _physics_process(delta: float) -> void:
 	 
 	showMiddle()
 	
-	
+	lastFrameTransform = global_transform
 
 func alignToAxis():
 	
@@ -67,14 +70,14 @@ func alignToAxis():
 	var objAAxis = -(objectAPoint.origin - objectAxisAPoint.origin)
 	var objectAAngleOffAxis = objAAxis.angle_to(middle.basis.z)
 	var objectAAxis
-	if objectAAngleOffAxis > 0:
+	if objectAAngleOffAxis > 0 and !nodeA.freeze:
 		objectAAxis = (objAAxis.cross(middle.basis.z)).normalized()
 		nodeA.rotate(objectAAxis,objectAAngleOffAxis )
 	
 	var objBAxis = -(objectBPoint.origin - objectAxisBPoint.origin)
 	var objectBAngleOffAxis = objBAxis.angle_to(middle.basis.z)
 	var objectBAxis
-	if objectBAngleOffAxis > 0:
+	if objectBAngleOffAxis > 0 and !nodeB.freeze:
 		objectBAxis = (objBAxis.cross(middle.basis.z)).normalized()
 		nodeB.rotate(objectBAxis,objectBAngleOffAxis)
 	
@@ -85,23 +88,50 @@ func showMiddle():
 	var objectAPoint = nodeA.global_transform * offsetA.affine_inverse()
 	var objectBPoint = nodeB.global_transform * offsetB.affine_inverse()
 	
+	Mathy.draw_line_between(get_tree(),objectAPoint.origin,nodeA.global_position,0.3,Color.RED)
+	Mathy.draw_line_between(get_tree(),objectBPoint.origin,nodeB.global_position,0.3,Color.BLUE)
+
 	
 	var middle = objectAPoint.interpolate_with(objectBPoint,0.5)
 	
 	if Input.is_action_pressed("q"):
 		return
 	
-	var aTarget = -(objectAPoint.origin - middle.origin) * TPS
-	var bTarget = -(objectBPoint.origin - middle.origin) * TPS
 	
-	var aDif = aTarget - nodeA.linear_velocity
-	var bDif = bTarget - nodeB.linear_velocity
 	
-	nodeA.linear_velocity += (aDif - bDif)/2.0
-	nodeB.linear_velocity += (bDif - aDif)/2.0
+	var aTarget = -(objectAPoint.origin - middle.origin) #* TPS
+	var bTarget = -(objectBPoint.origin - middle.origin) #* TPS
+	var aTargetDir = aTarget.normalized()
+	var bTargetDir = bTarget.normalized()
 	
-	nodeA.linear_velocity = aTarget
-	nodeB.linear_velocity = bTarget
+	#var aDif = aTarget - nodeA.linear_velocity
+	#var bDif = bTarget - nodeB.linear_velocity
+	
+	#nodeA.linear_velocity += (aDif - bDif)/2.0
+	#nodeB.linear_velocity += (bDif - aDif)/2.0
+	
+	#prints("before:",nodeB.linear_velocity)
+	
+	var velA = (
+		nodeA.linear_velocity - 
+		aTargetDir.dot(nodeA.linear_velocity) * aTargetDir +
+		-aTargetDir * (aTarget).length()
+		)
+	
+	nodeA.linear_velocity = (
+		nodeA.linear_velocity - 
+		-aTargetDir * (aTarget.length() -aTargetDir.dot(nodeA.linear_velocity))
+		)
+	
+	nodeB.linear_velocity = (
+		nodeB.linear_velocity - 
+		-bTargetDir * (bTarget.length() -bTargetDir.dot(nodeB.linear_velocity))
+		)
+	
+	prints(bTarget.length(),nodeB.linear_velocity.dot(bTargetDir))
+	#prints("after:",nodeB.linear_velocity)
+	
+	Mathy.draw_debug_sphere(get_tree(),nodeB.global_position + nodeB.linear_velocity/TPS,0.5,Color.LIME)
 	
 	#nodeA.rotation = middle.basis.get_euler()
 	#nodeB.rotation = middle.basis.get_euler()
@@ -110,5 +140,6 @@ func showMiddle():
 	Mathy.draw_transform(get_tree(),objectBPoint,1.0)
 	Mathy.draw_transform(get_tree(),middle,2.0)
 	
-	pass
+	
+	
 	
