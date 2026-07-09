b25543c6a7da8bcdbc2e8c2004f2d02517e5a00a
Deojee <72935900+Deojee@users.noreply.github.com>
2026-07-08 22:02:19 -0400

    make pinjoint to locations

--- scripts/PinJointPlus.gd
+++ scripts/PinJointPlus.gd
@@ -2,13 +2,12 @@ extends Node3D
 
 class_name PinJointPlus
 
-@export var nodeA : RigidBody3D
-@export var nodeB : RigidBody3D
+@export var node : RigidBody3D
+@export var pinPoint : Node3D
 
 var lastFrameTransform : Transform3D
 
-var offsetA : Transform3D
-var offsetB : Transform3D
+var offset : Transform3D
 
 #same as before but offset by 1 along the rotation axis
 #var axisOffsetA : Transform3D
@@ -45,54 +44,14 @@ func setup():
 	lastFrameTransform = global_transform
 	
 	#Node A in the hinge's local space
-	offsetA = global_transform.affine_inverse() * nodeA.global_transform
-	offsetB = global_transform.affine_inverse() * nodeB.global_transform
-	
-	
-	#axisOffsetA = global_transform.affine_inverse() * Transform3D(nodeA.global_transform.basis,nodeA.global_transform.origin + rotAxis)
-	#axisOffsetB = global_transform.affine_inverse() * Transform3D(nodeB.global_transform.basis,nodeB.global_transform.origin + rotAxis)
-	
-	#var offsetTransA = Transform3D(global_transform.basis,global_transform.origin + rotAxis)
-	
-	#axisOffsetA = offsetTrans.affine_inverse() * nodeA.global_transform
-	#axisOffsetB = offsetTrans.affine_inverse() * nodeB.global_transform
-	
-	nodeA.add_collision_exception_with(nodeB)
-	nodeB.add_collision_exception_with(nodeA)
+	offset = global_transform.affine_inverse() * node.global_transform
 	
 	name = str(numHinges) + "th hinge "
 	numHinges += 1
 	
 	
-	
-	if !bodiesDict.has(nodeA):
-		bodiesDict[nodeA] = [self]
-	else:
-		if !bodiesDict[nodeA].has(self):
-			bodiesDict[nodeA].append(self)
-	
-	if !bodiesDict.has(nodeB):
-		bodiesDict[nodeB] = [self]
-	else:
-		if !bodiesDict[nodeB].has(self):
-			bodiesDict[nodeB].append(self)
-	
 
 
-#returns the next hinge in the chain or null
-#only checks for other hinges on nodeB
-func getNextInChain():
-	for body in bodiesDict[nodeB]:
-		if body != self:
-			return body
-	return null
-
-func getAllNextInChain() -> Array:
-	var arr = []
-	for body in bodiesDict[nodeB]:
-		if body != self:
-			arr.append( body)
-	return arr
 
 var TPS:
 	get:
@@ -102,16 +61,14 @@ var TPS:
 
 func _physics_process(delta: float) -> void:
 	
-	var objectAPoint = nodeA.global_transform * offsetA.affine_inverse()
-	var objectBPoint = nodeB.global_transform * offsetB.affine_inverse()
-	var middle = objectAPoint.interpolate_with(objectBPoint,0.5)
+	var objectAPoint = node.global_transform * offset.affine_inverse()
 	
 	#Mathy.draw_line_between(get_tree(),objectAPoint.origin,nodeA.global_position,0.3,Color.RED)
 	#Mathy.draw_line_between(get_tree(),objectBPoint.origin,nodeB.global_position,0.3,Color.BLUE)
 	
 	MP.mark(objectAPoint.origin,0.6,Color.RED)
-	MP.mark(objectBPoint.origin,0.5,Color.GREEN)
-	MP.mark(middle.origin,0.4,Color.PURPLE)
+	
+	MP.mark(pinPoint.global_transform.origin,0.7,Color.GREEN)
 	
 	
 	for i in 1:
@@ -130,42 +87,17 @@ func update(del):
 	
 	lastFrameTransform = global_transform
 
-func getAPortion(del):
-	#if nodeA.freeze:
-	#	return 0.0
-	return (nodeB.mass / (nodeA.mass + nodeB.mass)) * del
-func getBPortion(del):
-	#if nodeB.freeze:
-	#	return 0.0
-	return (nodeA.mass / (nodeA.mass + nodeB.mass)) * del
-
-var aVel : Vector3
-var bVel : Vector3
-var aAng : Vector3
-var bAng : Vector3
 
 func showMiddle(del):
 	
-	var objectAPoint = nodeA.global_transform * offsetA.affine_inverse()
-	var objectBPoint = nodeB.global_transform * offsetB.affine_inverse()
+	var objectPoint = node.global_transform * offset.affine_inverse()
 	
-	#Mathy.draw_line_between(get_tree(),objectAPoint.origin,nodeA.global_position,0.3,Color.RED)
-	#Mathy.draw_line_between(get_tree(),objectBPoint.origin,nodeB.global_position,0.3,Color.BLUE)
-
 	
-	var middle = objectAPoint.interpolate_with(objectBPoint,0.5)
+	var target = pinPoint.global_position
 	
-	var aTarget = -(objectAPoint.origin - middle.origin) * TPS
-	var bTarget = -(objectBPoint.origin - middle.origin) * TPS
+	var targetVel = -(objectPoint.origin - target) * TPS
 	
-	nodeA.linear_velocity -= aVel * del
-	nodeB.linear_velocity -= bVel * del
-	
-	aVel = aTarget * 0.5 + bTarget * 0.5
-	bVel = bTarget * 0.5 + aTarget * 0.5
-	
-	nodeA.linear_velocity += aVel * del
-	nodeB.linear_velocity += bVel * del
+	node.linear_velocity = targetVel 
 	
 	
 	
