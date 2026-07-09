898ee81346e8a24a7e4d9d393b89355da1b2f81e
Deojee <72935900+Deojee@users.noreply.github.com>
2026-07-08 23:21:16 -0400

    cleaned up pinjoint a bit

--- scripts/PinJointPlus.gd
+++ scripts/PinJointPlus.gd
@@ -5,8 +5,6 @@ class_name PinJointPlus
 @export var nodeA : RigidBody3D
 @export var nodeB : RigidBody3D
 
-var lastFrameTransform : Transform3D
-
 var offsetA : Transform3D
 var offsetB : Transform3D
 
@@ -15,84 +13,29 @@ var offsetB : Transform3D
 #var axisOffsetB : Transform3D
 
 
-static var numHinges = 0
-
-static var bodiesDict : Dictionary = {}
-
-var rotAxis : Vector3:
-	get:
-		return global_basis.z
-	set(value):
-		print("can't change rot axis")
 
 func _ready() -> void:
 	
 	setup()
 	
 
-var isSetup = false
 func setup():
 	
-	if isSetup:
-		return
-	isSetup = true
 	
 	if !can_process():
 		return
 	
 	visible = true
 	
-	lastFrameTransform = global_transform
 	
 	#Node A in the hinge's local space
 	offsetA = global_transform.affine_inverse() * nodeA.global_transform
 	offsetB = global_transform.affine_inverse() * nodeB.global_transform
 	
 	
-	#axisOffsetA = global_transform.affine_inverse() * Transform3D(nodeA.global_transform.basis,nodeA.global_transform.origin + rotAxis)
-	#axisOffsetB = global_transform.affine_inverse() * Transform3D(nodeB.global_transform.basis,nodeB.global_transform.origin + rotAxis)
 	
-	#var offsetTransA = Transform3D(global_transform.basis,global_transform.origin + rotAxis)
-	
-	#axisOffsetA = offsetTrans.affine_inverse() * nodeA.global_transform
-	#axisOffsetB = offsetTrans.affine_inverse() * nodeB.global_transform
-	
-	nodeA.add_collision_exception_with(nodeB)
-	nodeB.add_collision_exception_with(nodeA)
-	
-	name = str(numHinges) + "th hinge "
-	numHinges += 1
-	
-	
-	
-	if !bodiesDict.has(nodeA):
-		bodiesDict[nodeA] = [self]
-	else:
-		if !bodiesDict[nodeA].has(self):
-			bodiesDict[nodeA].append(self)
-	
-	if !bodiesDict.has(nodeB):
-		bodiesDict[nodeB] = [self]
-	else:
-		if !bodiesDict[nodeB].has(self):
-			bodiesDict[nodeB].append(self)
 	
 
-
-#returns the next hinge in the chain or null
-#only checks for other hinges on nodeB
-func getNextInChain():
-	for body in bodiesDict[nodeB]:
-		if body != self:
-			return body
-	return null
-
-func getAllNextInChain() -> Array:
-	var arr = []
-	for body in bodiesDict[nodeB]:
-		if body != self:
-			arr.append( body)
-	return arr
 
 var TPS:
 	get:
@@ -100,7 +43,13 @@ var TPS:
 	set(value):
 		print("Please set TPS in physics/common")
 
-var enabled = false
+var enabled = false:
+	get:
+		return enabled
+	set(value):
+		if value:
+			setup()
+		enabled = value
 
 func _physics_process(delta: float) -> void:
 	
@@ -128,10 +77,6 @@ func update(del):
 	
 	showMiddle(del)
 	
-	
-	lastFrameTransform = global_transform
-
-
 
 
 
@@ -178,19 +123,7 @@ func alignToAxis(del):
 		
 	
 
-func getAngle():
-	
-	var objectAPoint = nodeA.global_transform * offsetA.affine_inverse()
-	var objectBPoint = nodeB.global_transform * offsetB.affine_inverse()
-	
-	var angleY = objectAPoint.basis.y.angle_to(objectBPoint.basis.y)
-	var angleX = objectAPoint.basis.x.angle_to(objectBPoint.basis.y)
-	
-	if angleX > PI/2.0:
-		return 2.0 * PI - angleY
-	else:
-		return angleY
-	
+
 
 func getAPortion(del):
 	if nodeA.freeze:
@@ -231,8 +164,6 @@ func showMiddle(del):
 	var bDif = bTarget - nodeB.linear_velocity
 	
 	var linearVel = (nodeA.linear_velocity + nodeB.linear_velocity)/20.0
-	
-	var divisor = 2.0
 	
 	nodeA.linear_velocity += (aDif) * getAPortion(del)
 	nodeB.linear_velocity -= (aDif) * getBPortion(del)
